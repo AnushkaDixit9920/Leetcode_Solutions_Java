00cd72030d701ec766461ef3e2e6a08ce5d2942e
Anushka Dixit <anushka036btcseai24@igdtuw.ac.in>
2025-12-07 20:42:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (5.41%) - LeetSync

---
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        if((high-low+1)%2==0){
+            return (high-low+1)/2;
+        }else{
+            if(high%2==0 && low%2==0){
+                return (high-low)/2;
+            }else if(high%2!=0 && low%2!=0){
+                return (high-low)/2+ 1;
+            }else if(high%2==0 && low%2!=0){
+                return (high-low)/2+1;
+            }else if(high%2!=0 && low%2==0){
+                return (high-low)/2+1;
+            }
+        }
+        return -1;
+    }
+}
