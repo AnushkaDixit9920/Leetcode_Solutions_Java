2c5d45397ddc7a2f721d99662095ce17c6ba3a5a
Anushka Dixit <anushka036btcseai24@igdtuw.ac.in>
2025-12-16 17:30:00 +0530

    Time: 3 ms (19.26%) | Memory: 84.4 MB (22.22%) - LeetSync

---
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.java
@@ -0,0 +1,23 @@
+class Solution {
+    public long getDescentPeriods(int[] prices) {
+        boolean continuous=false;
+        long ans=prices.length;
+        int prev=0;
+        for(int i=1;i<prices.length;i++){
+            if((prices[i]==prices[i-1]-1) && continuous==true){
+                ans+=2;
+                if(prev!=i-1){
+                    int len=i-prev+1;
+                    ans+=(len-3);
+                }
+            }else if(prices[i]==prices[i-1]-1){
+                ans+=1;
+                continuous=true;
+                prev=i-1;
+            }else{
+                continuous=false;
+            }
+        }
+        return ans;
+    }
+}
