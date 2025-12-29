09b3147196ceb2c49407197fd253a85f19346467
Anushka Dixit <anushka036btcseai24@igdtuw.ac.in>
2025-12-29 17:11:57 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (8.43%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
@@ -1,30 +1,16 @@
 class Solution {
     public int countNegatives(int[][] grid) {
         int count=0;
-        int m=grid.length;
-        int n=grid[0].length;
-        for(int i=0;i<m;i++){
-            int index=binarysearch(grid[i]);
-            if(index!=-1){
-                count+=(grid[i].length-index);
+        int m=grid.length-1;
+        int n=0;
+        while(m>=0 && n<grid[0].length){
+            if(grid[m][n]>=0){
+                n++;
+            }else if( grid[m][n]<0){
+                count+=grid[0].length-n;
+                m--;
             }
         }
         return count;
-
-    }
-    public int binarysearch(int[]arr){
-        int low=0;
-        int high=arr.length-1;
-        int ans=-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(arr[mid]<0){
-                ans=mid;
-                high=mid-1;
-            }else{
-                low=mid+1;
-            }
-        }
-        return ans;
     }
 }
