0823328b44fd9807ee204edfa66c05782380d9fa
Anushka Dixit <anushka036btcseai24@igdtuw.ac.in>
2026-01-25 18:20:48 +0530

    Time: 9 ms (26.64%) | Memory: 46.9 MB (65.13%) - LeetSync

--- 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.java
+++ 2112-minimum-difference-between-highest-and-lowest-of-k-scores/minimum-difference-between-highest-and-lowest-of-k-scores.java
@@ -3,13 +3,11 @@ class Solution {
         int ans=Integer.MAX_VALUE;
         Arrays.sort(nums);
         int n=nums.length;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<=n-k;i++){
             int j=i+k-1;
-            if(j<n){
-                int min=nums[i];
-                int max=nums[j];
-                ans=Math.min(max-min,ans);
-            }
+            int min=nums[i];
+            int max=nums[j];
+            ans=Math.min(max-min,ans);
         }
         return ans;
     }
