3e7474aabd93fd6339a59f36c252a40cd282237d
Anushka Dixit <anushka036btcseai24@igdtuw.ac.in>
2026-02-12 11:16:51 +0530

    Time: 1905 ms (5.05%) | Memory: 46.8 MB (89.70%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.java
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.java
@@ -2,11 +2,9 @@ class Solution {
     public int longestBalanced(String s) {
         int maxlen=Integer.MIN_VALUE;
         for(int i=0;i<s.length();i++){
-            int[]freq= new int[26];
             HashMap<Character,Integer>mp= new HashMap<>();
             for(int j=i;j<s.length();j++){
                 char ch=s.charAt(j);
-                freq[ch-'a']++;
                 mp.put(ch,mp.getOrDefault(ch,0)+1);
                 HashSet<Integer>st= new HashSet<>();
                 for(var e:mp.entrySet()){
