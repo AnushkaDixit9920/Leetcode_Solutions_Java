5abaf1c29a663b0c59a50cb92e6f656ed92c01d7
Anushka Dixit <anushka036btcseai24@igdtuw.ac.in>
2026-07-06 01:14:13 +0530

    Time: 8 ms (89.66%) | Memory: 44.5 MB (87.36%) - LeetSync

--- 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.java
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.java
@@ -1,53 +1,36 @@
-import java.util.*;
+import java.util.List;
 
 class Solution {
     public int[] pathsWithMaxScore(List<String> board) {
-        int n = board.size();
-        final long MOD = 1_000_000_007L;
+        int n = board.size(), MOD = 1_000_000_007;
+        int[][] dpSum = new int[n][n], dpCount = new int[n][n];
+        dpCount[n - 1][n - 1] = 1; // Start cell
         
-        char[][] b = new char[n][n];
-        for (int i = 0; i < n; i++) b[i] = board.get(i).toCharArray();
+        int[] dr = {1, 0, 1}, dc = {0, 1, 1}; // Up, Left, Up-Left transitions
         
-        int[][] dp = new int[n][n];      // max sum achievable from (i,j) to S... actually from S to (i,j)
-        long[][] cnt = new long[n][n];   // number of ways to achieve that max
-        for (int[] row : dp) Arrays.fill(row, -1); // -1 = unreachable
-        
-        dp[n-1][n-1] = 0;   // S itself contributes 0
-        cnt[n-1][n-1] = 1;
-        
-        int[] di = {1, 0, 1};   // down, right, down-right (i.e., the cells that can reach (i,j))
-        int[] dj = {0, 1, 1};
-        
-        // Process cells so that (i+1,j), (i,j+1), (i+1,j+1) are ready before (i,j).
         for (int i = n - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                if ((i == n-1 && j == n-1)) continue; // S already set
-                if (b[i][j] == 'X') continue;         // obstacle: unreachable
+                if ((i == n - 1 && j == n - 1) || board.get(i).charAt(j) == 'X') continue;
                 
-                int best = -1;
-                long ways = 0;
-                
-                for (int k = 0; k < 3; k++) {
-                    int ni = i + di[k], nj = j + dj[k];
-                    if (ni < n && nj < n && dp[ni][nj] >= 0) {
-                        if (dp[ni][nj] > best) {
-                            best = dp[ni][nj];
-                            ways = cnt[ni][nj];
-                        } else if (dp[ni][nj] == best) {
-                            ways = (ways + cnt[ni][nj]) % MOD;
+                int max = -1, count = 0;
+                for (int d = 0; d < 3; d++) {
+                    int r = i + dr[d], c = j + dc[d];
+                    if (r < n && c < n && dpCount[r][c] > 0) {
+                        if (dpSum[r][c] > max) {
+                            max = dpSum[r][c];
+                            count = dpCount[r][c];
+                        } else if (dpSum[r][c] == max) {
+                            count = (count + dpCount[r][c]) % MOD;
                         }
                     }
                 }
-                
-                if (best >= 0) {
-                    int val = (b[i][j] == 'E' || b[i][j] == 'S') ? 0 : (b[i][j] - '0');
-                    dp[i][j] = best + val;
-                    cnt[i][j] = ways;
+                if (max != -1) {
+                    char c = board.get(i).charAt(j);
+                    dpSum[i][j] = max + (c == 'E' ? 0 : c - '0');
+                    dpCount[i][j] = count;
                 }
             }
         }
-        
-        if (dp[0][0] < 0) return new int[]{0, 0};
-        return new int[]{dp[0][0], (int) cnt[0][0]};
+        return new int[]{dpSum[0][0], dpCount[0][0]};
     }
 }
