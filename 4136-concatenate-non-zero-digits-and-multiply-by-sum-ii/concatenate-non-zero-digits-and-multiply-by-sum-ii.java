7a8cd118cf5d8098eb884b81db7f57d08afc77fe
Anushka Dixit <anushka036btcseai24@igdtuw.ac.in>
2026-07-08 20:36:59 +0530

    Time: 17 ms (86.67%) | Memory: 129.1 MB (45.00%) - LeetSync

---
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int[] sumAndMultiply(String s, int[][] queries) {
+        int n = s.length();
+        int MOD = 1_000_000_007;
+        long[] pow10 = new long[n + 1];
+        pow10[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            pow10[i] = (pow10[i - 1] * 10) % MOD;
+        }
+        int[] sumD = new int[n + 1];
+        int[] cntN0 = new int[n + 1];
+        long[] p = new long[n + 1];
+        for (int i = 1; i <= n; i++) {
+            int d = s.charAt(i - 1) - '0';
+            sumD[i] = sumD[i - 1] + d;
+            cntN0[i] = cntN0[i - 1] + (d != 0 ? 1 : 0);
+            p[i] = (d != 0) ? (p[i - 1] * 10 + d) % MOD : p[i - 1];
+        }
+
+        int q = queries.length;
+        int[] ans = new int[q];
+
+        for (int j = 0; j < q; j++) {
+            int l = queries[j][0];
+            int r = queries[j][1];
+
+            int n0 = cntN0[r + 1] - cntN0[l];
+            int sd = sumD[r + 1] - sumD[l];
+            long x = (p[r + 1] - (p[l] * pow10[n0]) % MOD + MOD) % MOD;
+
+            ans[j] = (int) ((x * sd) % MOD);
+        }
+
+        return ans;
+    }
+}
