402d3ebfc29650e42ed7c241c2ee3a3d81c161e4
Anushka Dixit <anushka036btcseai24@igdtuw.ac.in>
2026-08-04 14:32:27 +0530

    Time: 8 ms (12.18%) | Memory: 33.6 MB (28.39%) - LeetSync

---
+++ 4107-find-missing-elements/find-missing-elements.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        unordered_set<int>present(nums.begin(),nums.end());
+        int min_val=*min_element(nums.begin(),nums.end());
+        int max_val=*max_element(nums.begin(),nums.end());
+        vector<int>result;
+        for(int i=min_val;i<=max_val;i++){
+            if(present.find(i)==present.end()){
+                result.push_back(i);
+            }
+        }
+        return result;
+    }
+};
